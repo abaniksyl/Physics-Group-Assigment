d2329e7728a7d02e91df13f707fb6ef45d3b46f0
Ashik Billah <148143394+abaniksyl@users.noreply.github.com>
2025-08-01 23:46:35 +0600

    Create Problem 1.c

---
+++ Problem 1.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double angle_θ, distance_d, fringe_order, lamda_λ;
+    int choice;
+
+    printf("Enter angle in Degree (θ°): ");
+    scanf("%lf", &angle_θ);
+    angle_θ = (angle_θ * 3.1416) / 180;
+
+    printf("Enter fringe order (m) : ");
+    scanf("%lf", &fringe_order);
+
+    printf("If you give the distance in (µm) enter : 1 or in (mm) enter 2 or in (m) enter 3 : ");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1:
+        printf("Enter distance in µm : ");
+        scanf("%lf", &distance_d);
+        lamda_λ = (distance_d * sin(angle_θ)) / fringe_order;
+        lamda_λ *= pow(10, 3);
+        printf("Wavelength lamda(λ) is %.2lf nanometer \n", lamda_λ);
+        break;
+
+    case 2:
+        printf("Enter distance in mm : ");
+        scanf("%lf", &distance_d);
+        lamda_λ = (distance_d * sin(angle_θ)) / fringe_order;
+        lamda_λ *= pow(10, 6);
+        printf("Wavelength lamda(λ) is %.2lf nanometer \n", lamda_λ);
+        break;
+
+    case 3:
+        printf("Enter distance in m : ");
+        scanf("%lf", &distance_d);
+        lamda_λ = (distance_d * sin(angle_θ)) / fringe_order;
+        lamda_λ *= pow(10, 9);
+        printf("Wavelength lamda(λ) is %.2lf nanometer \n", lamda_λ);
+        break;
+
+    default:
+        printf("invalid input");
+    }
+
+    if (lamda_λ <= 380 || lamda_λ >= 750)
+    {
+        printf("The Colour is Not Visible");
+    }
+
+    else if (lamda_λ >= 380 || lamda_λ <= 450)
+    {
+        printf("The Colour is Violet");
+    }
+
+    else if (lamda_λ >= 351 || lamda_λ <= 485)
+    {
+        printf("The Colour is Blue");
+    }
+
+    else if (lamda_λ >= 486 || lamda_λ <= 500)
+    {
+        printf("The Colour is Cyan");
+    }
+
+    else if (lamda_λ >= 501 || lamda_λ <= 565)
+    {
+        printf("The Colour is Green");
+    }
+
+    else if (lamda_λ >= 566 || lamda_λ <= 590)
+    {
+        printf("The Colour is Yellow");
+    }
+
+    else if (lamda_λ >= 591 || lamda_λ <= 625)
+    {
+        printf("The Colour is Orange");
+    }
+
+    else if (lamda_λ >= 626 || lamda_λ <= 750)
+    {
+        printf("The Colour is Red");
+    }
+    return 0;
+}
